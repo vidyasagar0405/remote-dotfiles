0a76d84e19e42b6772680f7a8e3da4edf590e8cd
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-01-31 23:47:29 +0530

    bash completion added

--- install-nvim.sh
+++ install-nvim.sh
@@ -19,3 +19,7 @@ chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 mkdir -p ~/.local/bin
 ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
+
+# Setup git completions for bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+echo "source ~/.git-completion.bash" >> ~/.bashrc
